dc0b1cb9f791120925a584a74ece0230e0ce86a8
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-09-12 10:01:01 +0200

    234

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-allAccounts = ["1212", "3311", "9999"]
+allAccounts = ["1212-12321", "3311-423", "9999-234234"]
 while True:
     print("1. Lista alla konton")
     print("2. Skriv ut hur många konton")
-    print("3. Lägg till konton")
-    print("4. Ta bort konton")
+    print("3. Lägg till konto")
+    print("4. Ta bort konto")
     print("5. Söka")
     action = input("Ange val:")
     if action == "2":
@@ -14,6 +14,14 @@ while True:
     elif action == "3":
         namn = input("Ange namn på nya kontot")
         allAccounts.append(namn)
+    elif action == "4":
+        for index in range(0, len(allAccounts)):
+            print(f"{index+1} {allAccounts[index]}")
+
+        index = int(input("Ange nummer på kontot som ska tas bort:"))
+        index = index -1
+        del allAccounts[index]
+
     elif action == "5":
         searchQuery = input("Sök efter:").lower()
         for namn in allAccounts:
